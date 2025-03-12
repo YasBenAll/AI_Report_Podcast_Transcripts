3c6b6349b181ced99473913d0d57d421cc8fce87
Yassin Ben Allal <yassinbenallal@hotmail.com>
2025-03-12 20:42:14 +0100

    Add github button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -89,6 +89,13 @@ export default function Home() {
         <title>AI report Transcripts</title>
         <meta property="og:title" content="AI Report Transcripts" key="title" />
       </Head>
+      <div className="absolute right-0 p-4">
+          <a href="https://github.com/YasBenAll/AI_Report_Podcast_Transcripts" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 hover:bg-blue-100 rounded-md">
+            <svg className="w-8 h-8 fill-current text-black hover:text-blue-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.113.82-.263.82-.583 0-.288-.01-1.05-.015-2.06-3.338.725-4.042-1.61-4.042-1.61-.546-1.385-1.333-1.755-1.333-1.755-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.238 1.84 1.238 1.07 1.835 2.805 1.305 3.49.998.108-.775.42-1.305.76-1.605-2.665-.305-5.466-1.335-5.466-5.93 0-1.31.47-2.38 1.235-3.22-.125-.305-.535-1.53.115-3.18 0 0 1.005-.322 3.3 1.23.955-.265 1.98-.398 3-.403 1.02.005 2.045.138 3 .403 2.28-1.552 3.285-1.23 3.285-1.23.655 1.65.245 2.875.12 3.18.77.84 1.235 1.91 1.235 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.815 1.102.815 2.222 0 1.605-.015 2.895-.015 3.285 0 .32.215.705.825.585C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z"/>
+            </svg>
+          </a>
+      </div>      
       <div className="flex justify-center">
         <div className="bg-opacity-10 p-30 px-6 py-6 rounded-md mx-auto max-w-3/4 w-2/3 mt-8">
           <div className="flex justify-center">
